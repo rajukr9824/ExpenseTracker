aee917a52a3d539a0e1be12bda9aa662756582eb
Raju Kumar <rajuk.ug22.cs@nitp.ac.in>
2024-08-16 16:33:57 +0530

    security increased
    password hashing used

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,32 @@
 const User = require("../models/userModel")
-
+const bcrypt=require('bcrypt')
 
 const loginController=async(req, res)=>{
     try {
         const {email, password}=req.body
-      const user = await User.findOne({email, password})
-        if(!user){
-            return res.status(404).send('User not found!')
+        if(!email || !password){
+            return res.status(401).send({
+                 success:false,
+                 message:'Please provide email and password!'
+            })
         }
-        res.status(200).json({
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(401).send({
+                success:false,
+                message:'user is not registered!'
+            })
+        }
+        
+        const isMatch=await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(401).send({
+                success:false,
+                message:'Invalid username or password!'
+            })
+        }
+       
+       return res.status(200).json({
             success:true,
             user,
         })
@@ -22,12 +40,22 @@ const loginController=async(req, res)=>{
 
 const registerController=async(req,res)=>{
     try {
-        const newUser=new User(req.body)
-        await newUser.save();
-        res.status(201).json({
+        const {name, email, password} = req.body
+        const existingUser=await User.findOne({email})
+        if(existingUser){
+            return res.status(401).send({
+                success:false,
+            
+            })
+        }
+      const hashedPassword=await bcrypt.hash(password, 10)
+        const user=new User({name, email, password:hashedPassword})
+        await user.save()
+        return res.status(201).json({
             success:true,
-            newUser,
+            user
         })
+    
     } catch (error) {
         res.status(400).json({
             success:false,
